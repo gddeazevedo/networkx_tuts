02ebf3df98f87e423c480ef77c29dd767617ac11
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-14 11:53:24 -0300

    adding multigraph example

--- examples/multigraph.py
+++ examples/multigraph.py
@@ -1,6 +1,22 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
-mg = nx.MultiDiGraph()
-mg.add_weighted_edges_from([(1, 2, 0.5), (1, 2, 0.75), (2, 3, 0.5)])
-print(dict(mg.degree(weight='weight')))
+mg = nx.MultiGraph()
+nodes = [1, 2, 3, 4, 5, 6]
+edges = [
+    (1, 2),
+    (1, 3),
+    (1, 6),
+    (2, 4),
+    (5, 6),
+    (3, 4),
+    (4, 5),
+    (5, 4)
+]
+
+mg.add_nodes_from(nodes)
+mg.add_edges_from(edges)
+
+nx.draw(mg, with_labels=True, font_weight='bold')
+plt.show()
