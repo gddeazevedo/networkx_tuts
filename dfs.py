d7a1267899c3b0cc78ea1b828a4827e28efea8f0
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-21 13:45:10 -0300

    implementing dfs

--- dfs.py
+++ dfs.py
@@ -1,5 +1,5 @@
 import networkx as nx
-
+import matplotlib.pyplot as plt
 
 BLACK = 'B'
 WHITE = 'W'
@@ -12,15 +12,20 @@ pi = {}
 colors = {}
 time = 0
 
-nodes = [1, 2, 3, 4, 5, 6]
+nodes = [1, 2, 3, 4, 5, 6, 7]
 edges = [
     (1, 2),
-    (1, 3),
-    (2, 4),
-    (5, 6),
+    (2, 3),
+    (2, 6),
+    (6, 7),
+    (7, 6),
+    (4, 1),
     (3, 4),
+    (5, 4),
+    (5, 3),
+    (3, 7)
 ]
-G = nx.Graph()
+G = nx.DiGraph()
 
 G.add_nodes_from(nodes)
 G.add_edges_from(edges)
@@ -42,7 +47,7 @@ def dfs_visit(G: nx.Graph, u: int):
     f[u] = time
 
 
-def dfs(G: nx.Graph):
+def dfs(G: nx.Graph | nx.DiGraph):
     for u in G.nodes:
         pi[u] = None
         colors[u] = WHITE
@@ -57,3 +62,17 @@ dfs(G)
 print(pi)
 print(d)
 print(f)
+
+tree = nx.DiGraph()
+
+tree.add_nodes_from(pi.keys())
+
+for key, value in pi.items():
+    if value != None:
+        tree.add_edge(value, key)
+
+
+nx.draw(G, with_labels=True)
+plt.show()
+nx.draw(tree, with_labels=True)
+plt.show()
